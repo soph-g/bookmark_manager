53f8cb9b6ce724adedb6409270d8bc35aaa14b3a
Sophie Gill <sophie-gill@hotmail.com>
2016-07-27 12:07:30 +0100

    adding a one to many tag option

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,3 @@
-require 'data_mapper'
-require 'dm-postgres-adapter'
-
 class Link
   include DataMapper::Resource
 
@@ -8,8 +5,6 @@ class Link
   property :title,  String
   property :url,    String
 
-end
+  has n, :tags, through: Resource
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-DataMapper.finalize
-DataMapper.auto_upgrade!
+end
