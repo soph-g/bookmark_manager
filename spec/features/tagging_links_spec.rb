44cc7b78bc21de3e9d1b591849a22fd7bb015f93
lukew244 <luke.webster86@gmail.com>
2016-07-28 13:43:13 +0100

    Adds multiple tagging and rakefile

--- spec/features/tagging_links_spec.rb
+++ spec/features/tagging_links_spec.rb
@@ -9,4 +9,14 @@ feature 'Tagging links' do
     link = Link.first
     expect(link.tags.map(&:name)).to(include('adventure'))
   end
-end
+
+  scenario 'adds multiple tags to a link' do
+    visit '/links/new'
+    fill_in 'url', with: 'http://www.zombo.com/'
+    fill_in 'title', with: 'This is Zombocom'
+    fill_in 'tags', with: 'adventure mystery'
+    click_button 'Create link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to(include('adventure', 'mystery'))
+  end
+  end
