c377a22df670b6ad7f3c28acd2cf4b5e06342056
Thady Condon <thadycondon@hotmail.com>
2016-07-29 12:20:02 +0100

    Refactored controller into related files

--- app/app.rb
+++ app/app.rb
@@ -1,86 +1,12 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
-require_relative 'data_mapper_setup'
 require 'sinatra/flash'
+require 'sinatra/partial'
 
-class BookmarkManager < Sinatra::Base
-
-  register Sinatra::Flash
-
-  use Rack::MethodOverride
-
-  enable :sessions
-  set :session_secret, 'super secret'
-
-  get '/links' do
-    @links = Link.all
-    erb :'links/index'
-  end
-
-  get '/links/new' do
-    erb :'links/new'
-  end
-
-  post '/links' do
-    link = Link.create( url: params[:url],
-                        title: params[:title] )
-    params[:tags].split.each do |tag|
-      link.tags << Tag.first_or_create(name: tag)
-    end
-    link.save
-    redirect '/links'
-  end
-
-  get '/tags/:name' do
-    tag = Tag.first(name: params[:name])
-    @links = tag ? tag.links : []
-    erb :'links/index'
-  end
-
-  get '/users/new' do
-    erb :'users/new'
-  end
-
-  post '/users' do
-    @user = User.create(email: params[:email],
-                password: params[:password],
-                password_confirmation: params[:password_confirmation])
-    if @user.save
-      session[:user_id] = @user.id
-      redirect(to('/links'))
-    else
-      flash.now[:errors] = @user.errors.full_messages
-      erb :'users/new'
-    end
-  end
-
-  helpers do
-    def current_user
-      @current_user ||= User.get(session[:user_id])
-    end
-  end
-
-  get '/sessions/new' do
-    erb :'sessions/new'
-  end
-
-  post '/sessions' do
-    user = User.authenticate(params[:email], params[:password])
-    if user
-      session[:user_id] = user.id
-      redirect to('/links')
-    else
-      flash.now[:errors] = ['The email or password is incorrect']
-      erb :'sessions/new'
-    end
-  end
-
-  delete '/sessions' do
-    session[:user_id] = nil
-    flash.keep[:notice] = 'Goodbye!'
-    redirect to '/links'
-  end
-
-  run! if app_file == $0
-end
+require_relative 'data_mapper_setup'
+require_relative 'server'
+require_relative 'controller/links'
+require_relative 'controller/sessions'
+require_relative 'controller/tags'
+require_relative 'controller/users'
